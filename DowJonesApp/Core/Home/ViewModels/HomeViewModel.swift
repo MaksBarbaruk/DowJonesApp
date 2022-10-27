80e45f8a50e5189bbe9859b94c84c8583b90ea47
Maks Barbaruk <maksbarbaruk@gmail.com>
2022-10-27 15:10:05 +0300

    filterAddedToSearch

--- DowJonesApp/Core/Home/ViewModels/HomeViewModel.swift
+++ DowJonesApp/Core/Home/ViewModels/HomeViewModel.swift
@@ -21,11 +21,34 @@ class HomeViewModel: ObservableObject {
     }
     
     func addSubscribers() {
+        /*
         dataService.$allCoins
             .sink { [weak self] (returnedCoins) in
                 self?.allCoins = returnedCoins
             }
             .store(in: &cancellables)
-
+        */
+        $searchText
+            .combineLatest(dataService.$allCoins)
+            .debounce(for: .seconds(0.3), scheduler: DispatchQueue.main)
+            .map(filterCoins)
+            .sink { [weak self] (returnedCoins) in
+                self?.allCoins = returnedCoins
+            }
+            .store(in: &cancellables)
+    }
+    
+    private func filterCoins(text: String, coins: [Coin]) -> [Coin] {
+        guard !text.isEmpty else {
+            return coins
+        }
+        
+        let lowercasedText = text.lowercased()
+        let filteredCoins = coins.filter { (coin) -> Bool in
+            return coin.name.lowercased().contains(lowercasedText) ||
+                coin.symbol.lowercased().contains(lowercasedText) ||
+                coin.id.lowercased().contains(lowercasedText)
+        }
+        return filteredCoins
     }
 }
